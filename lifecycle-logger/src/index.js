939674ef746f79ba12123dcfd2eecddf5a9028c3
Guinslym <guinslym@gmail.com>
2017-08-12 16:34:33 -0400

    ch01

--- lifecycle-logger/src/index.js
+++ lifecycle-logger/src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+//
+import { loggerExample }  from './loggerExample'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<loggerExample />, document.getElementById('root'))
+registerServiceWorker()
